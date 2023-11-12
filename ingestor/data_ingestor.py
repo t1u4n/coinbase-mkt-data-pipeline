0cb394b744ffe286f394f99257c1b132295f6b68
Tian Luan <tiluan1999@hotmail.com>
2023-11-11 23:11:25 -0800

    feat: Add logic to send message to Kafka

--- ingestor/data_ingestor.py
+++ ingestor/data_ingestor.py
@@ -2,29 +2,38 @@ import websocket
 import json
 from threading import Thread
 from typing import List
+from dotenv import load_dotenv
+import os
+from kafka import KafkaProducer
+
+load_dotenv()
 
 COINBASE_MKT_WEBSOCKET_API_PROD = 'wss://ws-feed.exchange.coinbase.com'
 COINBASE_MKT_WEBSOCKET_API_SANDBOX = 'wss://ws-feed-public.sandbox.exchange.coinbase.com'
-
+KAFKA_URL = os.getenv('KAFKA_URL')
 
 
 class CoinbaseDataIngestor:
     """This class is used to ingest data from Coinbase."""
 
     def __init__(self, url: str, channels: List[str], product_ids: List[str]):
-        """Constructor of YFinanceDataIngestor."""
+        """Constructor of CoinbaseDataIngestor."""
         self._url = url
-        self.channels = channels
-        self.product_ids = product_ids
+        self._channels = channels
+        self._product_ids = product_ids
         self._ws = websocket.WebSocketApp(self._url,
                                          on_open=self._on_open,
                                          on_message=self._on_message,
                                          on_error=self._on_error,
                                          on_close=self._on_close)
+        self._kafka_producer = KafkaProducer(bootstrap_servers = [KAFKA_URL])
     
     def _on_message(self, ws: 'websocket.WebSocketApp', msg: str) -> None:
         """This method is used to handle the message received from Coinbase."""
-        print(f'Msg: {msg}')
+        payload = json.loads(msg)
+        if 'type' in payload and payload['type'] == 'ticker':
+            topic = payload['product_id']
+            self._kafka_producer.send(topic, msg.encode('utf-8'))
     
     def _on_error(self, ws: 'websocket.WebSocketApp', err: str) -> None:
         """This method is used to handle the error received from Coinbase."""
@@ -40,11 +49,11 @@ class CoinbaseDataIngestor:
         def run(*args):
             subscribe_message = {
                 "type": "subscribe",
-                "channels": self.channels,
-                "product_ids": self.product_ids
+                "channels": self._channels,
+                "product_ids": self._product_ids
             }
             ws.send(json.dumps(subscribe_message))
-            print("Subscribed to channels:", self.channels)
+            print("Subscribed to channels:", self._channels)
         Thread(target=run).start()
 
     def run(self) -> None:
@@ -54,9 +63,12 @@ class CoinbaseDataIngestor:
     def shutdown(self) -> None:
         """This method is used to shutdown the websocket gracefully."""
         self._ws.close(1000, 'Shutdown')
+        self._kafka_producer.close(10)
 
 
 if __name__ == '__main__':
+    # Enable traces for websocket client if need to debug
+    # websocket.enableTrace(True)
     url = "wss://ws-feed.exchange.coinbase.com"
     channels = ["ticker"]
     product_ids = ["BTC-USD"]
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Coinbase Market Data Pipeline
+A data pipeline involves Apache Kafka, Apache Spark, Apache Airflow and Amazon S3 to ingest, process and store market data from public Coinbase Market API.
